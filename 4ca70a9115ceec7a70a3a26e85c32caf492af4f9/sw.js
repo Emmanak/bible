f511d3cc1b033a9bb9f51a7069f3e816f93ecf03
Emmanuel Augustine <emmanuelaugustine@hotmail.com>
2020-05-12 21:26:35 -0700

    PWA Test with absolute path  10

--- 4ca70a9115ceec7a70a3a26e85c32caf492af4f9/sw.js
+++ 4ca70a9115ceec7a70a3a26e85c32caf492af4f9/sw.js
@@ -1,15 +1,7 @@
 const staticCacheName = 'site-content';
 const assets = [
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/index.html',
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/frames.htm',
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/icons/icon-128x128.png',
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/icons/icon-144x144.png',
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/icons/icon-152x152.png',
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/icons/icon-192x192.png',
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/icons/icon-384x384.png',
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/icons/icon-512x512.png',
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/icons/icon-72x72.png',
-'https://emmanak.github.io/bible/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/icons/icon-96x96.png',
+'/4ca70a9115ceec7a70a3a26e85c32caf492af4f9/index.html',
+'/frames.htm',
 
 
 ];
